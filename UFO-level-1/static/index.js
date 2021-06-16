470713cc18efe76920d65acee511a5916a2ced0a
lmodenbach <lmodenbach@gmail.com>
2021-06-16 15:54:49 -0700

    got data and fixed access to my styling
    need to get the bootstrap classes straight still

--- UFO-level-1/static/index.js
+++ UFO-level-1/static/index.js
@@ -2,26 +2,27 @@ var sightings = data;
 
 var body = d3.select("body");
 
-var table = body.append("table").style("background-color", "powderblue").style("width", "500px");
-table.attr("class", "table table-hover table-bordered");
+var table = body.append("table");
+table.attr("class", "table table-bordered table-hover");
+//table.attr("class", "table");
 
 var thead = table.append("thead");
 
 var row = thead.append("tr");
 
-var headers = ["date/time", "city", "state", "country", "shape", "comment"];
+var headers = ["Date/Time", "City", "State", "Country", "Shape", "Duration", "Comment"];
 
 headers.forEach((header) => {
     var cell = row.append("th");
      cell.text(header);
   });
 
-/* data.forEach((sighting) => {
-    var row = tbody.append("tr");
+data.forEach((sighting) => {
+    var row = table.append("tr");
     Object.entries(sighting).forEach(([key, value]) => {
     var cell = row.append("td");
     cell.text(value);
     });
-  }); */
+  });
 
  
