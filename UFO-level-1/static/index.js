2a4c6626454ccc28822f22c51e21cae892d14c08
lmodenbach <lmodenbach@gmail.com>
2021-06-16 14:34:49 -0700

    empty table with headers appears

--- UFO-level-1/static/index.js
+++ UFO-level-1/static/index.js
@@ -1,1 +1,26 @@
 var sightings = data;
+
+var body = d3.select("body");
+
+var table = body.append("table").style("background-color", "powderblue");
+
+var thead = table.append("thead");
+
+var row = thead.append("tr");
+
+var headers = ["date/time", "city", "state", "country", "shape", "comment"];
+
+headers.forEach((header) => {
+    var cell = row.append("th");
+     cell.text(header);
+  });
+
+/* data.forEach((sighting) => {
+    var row = tbody.append("tr");
+    Object.entries(sighting).forEach(([key, value]) => {
+    var cell = row.append("td");
+    cell.text(value);
+    });
+  }); */
+
+ 
