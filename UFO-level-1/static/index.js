178dd05a7a6e95e56519e9e86a36cb54e9854021
lmodenbach <lmodenbach@gmail.com>
2021-06-16 16:46:23 -0700

    got hover and bordered, need sizing again

--- UFO-level-1/static/index.js
+++ UFO-level-1/static/index.js
@@ -3,8 +3,7 @@ var sightings = data;
 var body = d3.select("body");
 
 var table = body.append("table");
-table.attr("class", "table table-bordered table-hover");
-//table.attr("class", "table");
+table.attr("class", "table table-hover table-bordered");
 
 var thead = table.append("thead");
 
@@ -17,8 +16,10 @@ headers.forEach((header) => {
      cell.text(header);
   });
 
+var tbody = table.append("tbody");
+
 data.forEach((sighting) => {
-    var row = table.append("tr");
+    var row = tbody.append("tr");
     Object.entries(sighting).forEach(([key, value]) => {
     var cell = row.append("td");
     cell.text(value);
