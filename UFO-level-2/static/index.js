575ec3a20a280ea3be61d0f65a8d3d63f5a9df39
lmodenbach <lmodenbach@gmail.com>
2021-06-16 22:02:55 -0700

    updated level 2 with level 1

--- UFO-level-2/static/index.js
+++ UFO-level-2/static/index.js
@@ -1,35 +1,56 @@
 var sightings = data;
+createTable(sightings);
 
-var body = d3.select("body");
+function createTable(data) {
+    var body = d3.select("body");
+    var table = body.append("table");
+    table.attr("class", "table table-hover table-bordered");
 
-var table = body.append("table");
-table.attr("class", "table table-hover table-bordered");
-
-var thead = table.append("thead");
-var row = thead.append("tr");
-
-var headers = ["Date/Time", "City", "State", "Country", "Shape", "Duration", "Comment"];
-
-headers.forEach((header) => {
-    var cell = row.append("th");
-    cell.text(header);
-  });
-
-var tbody = table.append("tbody");
-
-data.forEach((sighting) => {
-    var row = tbody.append("tr");
-    Object.entries(sighting).forEach(([key, value]) => {
-    var cell = row.append("td");
-    cell.text(value);
+    var thead = table.append("thead");
+    var row = thead.append("tr");
+    var headers = ["Date/Time", "City", "State", "Country", "Shape", "Duration", "Comment"];
+    headers.forEach((header) => {
+        var cell = row.append("th");
+        cell.text(header);
     });
-});
 
+    var tbody = table.append("tbody");
+    data.forEach((sighting) => {
+        var row = tbody.append("tr");
+        Object.entries(sighting).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+        });
+    });
+}
 var button = d3.select("#button");
 var form = d3.select("#form");
   
 button.on("click", searchTable);
 form.on("submit", searchTable);
+
+function searchTable() {
+
+    d3.event.preventDefault();
+    
+    var inputElement = d3.select("#sightings-form-input");
+    var inputValue = inputElement.property("value");
+
+    var filtered = sightings.filter(sighting => sighting.datetime === inputValue);
+    
+    d3.selectAll("table").remove();
+    if (filtered.length > 0)
+        createTable(filtered); 
+    else
+        createTable(sightings);
+}
+
+
+
+
+
+
+
 
 function searchTable() {
 
