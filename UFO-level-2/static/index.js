4bf96a625d7fd90d1edc6a3b35cb5ae4f1cbad0a
lmodenbach <lmodenbach@gmail.com>
2021-06-16 19:26:50 -0700

    import UFO-level-1 into UFO-level-2

---
+++ UFO-level-2/static/index.js
@@ -0,0 +1,55 @@
+var sightings = data;
+
+var body = d3.select("body");
+
+var table = body.append("table");
+table.attr("class", "table table-hover table-bordered");
+
+var thead = table.append("thead");
+
+var row = thead.append("tr");
+
+var headers = ["Date/Time", "City", "State", "Country", "Shape", "Duration", "Comment"];
+
+headers.forEach((header) => {
+    var cell = row.append("th");
+    cell.text(header);
+  });
+
+var tbody = table.append("tbody");
+
+data.forEach((sighting) => {
+    var row = tbody.append("tr");
+    Object.entries(sighting).forEach(([key, value]) => {
+    var cell = row.append("td");
+    cell.text(value);
+    });
+});
+
+var button = d3.select("#button");
+var form = d3.select("#form");
+  
+button.on("click", searchTable);
+form.on("submit", searchTable);
+
+function searchTable() {
+
+    d3.event.preventDefault();
+    
+    var inputElement = d3.select("#sightings-form-input");
+    var inputValue = inputElement.property("value");
+
+    var filtered = sightings.filter(sighting => sighting.datetime === inputValue);
+    var resultsDiv = d3.select(".summary");
+
+    resultsDiv.html("");
+
+    filtered.forEach((sighting) => {
+        Object.entries(sighting).forEach(([key, value]) => {
+        resultsDiv.append("h5").text(`${key}: ${value}`).style("color", "blueviolet");
+        });
+        resultsDiv.append("h5").text("--------------------------------------------").style("color", "blueviolet");
+    });
+     
+    
+}
